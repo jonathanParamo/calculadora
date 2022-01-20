a5747a7e7ac1d6ba7899a5d9ac060c7831e4b610
Your Jonathan Paramo <jhonathan-and@outlook.com>
2022-01-20 14:18:31 -0500

    reescribi la condicional if. para que no genere nan al no escribir un numero o los dos

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,7 @@
 function resta(){
   const num = parseFloat(document.getElementById("num_uno").value);
   const num1 = parseFloat(document.getElementById("num_dos").value);
+  if(num && num1){
     const resultado = document.getElementById("resultado");
     const res = num - num1;
     const imprimir =
@@ -8,12 +9,13 @@ function resta(){
     <p> Resultado: ${res} </p>
     `
     resultado.innerHTML = imprimir;
-    
+  }
 }
 
 function suma(){
   const num = parseFloat(document.getElementById("num_uno").value);
   const num1 = parseFloat(document.getElementById("num_dos").value); 
+  if(num && num1){
     const resultado = document.getElementById("resultado");
     const sum = num + num1;
     const imprimir = 
@@ -21,12 +23,13 @@ function suma(){
     <p> Resultado: ${sum} </p>
     `
     resultado.innerHTML = imprimir;
-  
+  }
 }
 
 function multiplicacion(){
   const num = parseFloat(document.getElementById("num_uno").value);
   const num1 = parseFloat(document.getElementById("num_dos").value);
+  if(num && num1){
     const resultado = document.getElementById("resultado");
     const mul = num * num1;
     const imprimir = 
@@ -34,16 +37,19 @@ function multiplicacion(){
     <p> Resultado: ${mul} </p>
     `
     resultado.innerHTML = imprimir;
+  }
 }
 
 function division(){
   const num = parseFloat(document.getElementById("num_uno").value);
   const num1 = parseFloat(document.getElementById("num_dos").value);
-  const resultado = document.getElementById("resultado");
+  if(num && num1){
+    const resultado = document.getElementById("resultado");
     const div = num / num1;
     const imprimir =
     `
     <p> Resultado: ${div} </p>
     `
     resultado.innerHTML = imprimir;
+  }
 }
