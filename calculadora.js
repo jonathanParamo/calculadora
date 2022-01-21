9aec48a9180078601910a92e2b93319a0ac80b65
Your Jonathan Paramo <jhonathan-and@outlook.com>
2022-01-21 13:54:43 -0500

    se mejora las funciones, al recibir los numeros por medio de otra funcion, se agg un boton de borrar, aun sin funcionalidad

--- calculadora.js
+++ calculadora.js
@@ -1,55 +1,51 @@
-function resta(){
+function recibir_numeros(){
   const num = parseFloat(document.getElementById("num_uno").value);
   const num1 = parseFloat(document.getElementById("num_dos").value);
-  if(num && num1){
-    const resultado = document.getElementById("resultado");
-    const res = num - num1;
-    const imprimir =
-    `
-    <p> Resultado: ${res} </p>
-    `
-    resultado.innerHTML = imprimir;
+
+  const numeros = {
+    num,
+    num1,
   }
+  return numeros;
 }
 
 function suma(){
-  const num = parseFloat(document.getElementById("num_uno").value);
-  const num1 = parseFloat(document.getElementById("num_dos").value); 
+  let {num, num1} = recibir_numeros();
   if(num && num1){
     const resultado = document.getElementById("resultado");
     const sum = num + num1;
-    const imprimir = 
-    `
-    <p> Resultado: ${sum} </p>
-    `
+    const imprimir = `<p> Resultado: ${sum} </p>`
     resultado.innerHTML = imprimir;
   }
 }
 
+function resta(){
+  const {num, num1} = recibir_numeros();
+  if(num && num1){
+    const resultado = document.getElementById("resultado");
+    const res = num - num1;
+    const imprimir =`<p> Resultado: ${res} </p>`
+    resultado.innerHTML = imprimir;
+  }
+}
+
+
 function multiplicacion(){
-  const num = parseFloat(document.getElementById("num_uno").value);
-  const num1 = parseFloat(document.getElementById("num_dos").value);
+  const {num, num1} = recibir_numeros();
   if(num && num1){
     const resultado = document.getElementById("resultado");
     const mul = num * num1;
-    const imprimir = 
-    `
-    <p> Resultado: ${mul} </p>
-    `
+    const imprimir = `<p> Resultado: ${mul} </p>`
     resultado.innerHTML = imprimir;
   }
 }
 
 function division(){
-  const num = parseFloat(document.getElementById("num_uno").value);
-  const num1 = parseFloat(document.getElementById("num_dos").value);
+  const {num, num1} = recibir_numeros();
   if(num && num1){
     const resultado = document.getElementById("resultado");
     const div = num / num1;
-    const imprimir =
-    `
-    <p> Resultado: ${div} </p>
-    `
+    const imprimir =`<p> Resultado: ${div} </p>`
     resultado.innerHTML = imprimir;
   }
 }
